7ac4411ef7fea30eac1659da36c805aef90780d3
Amelia <amy@tachikoma.local>
2019-01-30 00:31:21 -0500

    111/663

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,4 @@
-import csv
-#from requests_html import HTMLSession
-import requests
+import csv,sys,re,requests
 from bs4 import BeautifulSoup
 
 def test_selector2(url,sel):
@@ -12,24 +10,28 @@ def test_selector2(url,sel):
 def test_selector(url,sel):
     r = requests.get(url)
     soup = BeautifulSoup(r.text,'html.parser')
-    return [i.text for i in soup.select(sel)]
+    return [re.sub(r"\s{2,}","",i.text) for i in soup.select(sel)]
 
-def processor(row):
+def processor(row,index):
     url = row["website"]
-    a = input(f"{url} : ")
     try:
-        print(test_selector(url,a))
-    except:
-        pass
-    b = input("did it work? (Y/n): ")
-    if b not in ["Y","y",""]:
-        return processor(row)
-    else:
-        return a
+        a = input(f"{url} ({index}/662) : ")
+        try:
+            print(test_selector(url,a))
+        except:
+            pass
+        b = input("did it work? (Y/n): ")
+        if b not in ["Y","y",""]:
+            return processor(row,index)
+        else:
+            return a
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 def main():
     fn = ["callsign","lat","lon","website","logo"]
     fns = fn+["selector"]
+    index = 0
     with open("affiliate.csv","r") as f:
         with open("aff-final.csv","r+") as j:
             w = csv.DictWriter(j,fieldnames=fns)
@@ -38,9 +40,10 @@ def main():
             hasHit = False
             r.__next__()
             for line in r:
+                index += 1
                 if hasHit:
                     if line["website"]:
-                        line["selector"] = processor(line)
+                        line["selector"] = processor(line,index)
                         w.writerow(line)
                 elif line["website"] == lastSite[0]["website"]:
                     hasHit = True
